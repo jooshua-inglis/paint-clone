ffda971ee9efd7cc8817f57652c533757db95e6d
Mikhayla Stephenson <mstephenson1999@hotmail.com>
2019-05-17 17:31:38 +1000

    Ellipse class re-implemented

--- src/vector/util/Tool.java
+++ src/vector/util/Tool.java
@@ -1,13 +1,9 @@
 package vector.util;
 
-import vector.shape.Rectangle;
+import vector.shape.*;
 //import vector.shape.Ellipse;
 //import vector.shape.Line;
-import vector.shape.Plot;
-import vector.shape.Polygon;
 
-
-import vector.shape.VectorShape;
 
 public enum Tool {
     RECTANGLE,
@@ -22,11 +18,11 @@ public enum Tool {
             case RECTANGLE:
                 return new Rectangle();
             case ELLIPSE:
-               // return new Ellipse();
+                return new Ellipse();
             case POLYGON:
                 return new Polygon();
            case LINE:
-               //return new Line();
+               return new Line();
            case PLOT:
                return new Plot();
             default:
